20ab84176a2df04ddeca6396067525d2d631ce45
LuisDi98 <aguilarluisdi@gmail.com>
2025-06-10 11:21:43 -0600

    fix: email spam

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -10,8 +10,8 @@ export interface MailOptions {
 export const sendEmail = async (mailOptions: MailOptions) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.hostinger.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: "info@urbania-custom.com",
       pass: "UrbaniaInfo2025!!",
